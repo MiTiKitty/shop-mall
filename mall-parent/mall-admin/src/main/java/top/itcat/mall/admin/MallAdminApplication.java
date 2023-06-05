27746f0167f52e5bc008963585260f1377173431
CatKitty <3364187323@qq.com>
2023-06-05 12:46:37 +0800

    搭建admin模块基本骨架

---
+++ mall-parent/mall-admin/src/main/java/top/itcat/mall/admin/MallAdminApplication.java
@@ -0,0 +1,20 @@
+package top.itcat.mall.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className: MallAdminApplication <br/>
+ * @description: 启动类 <br/>
+ * @author: CatKitty 33641 <br/>
+ * @date: 2023/06/05 <br/>
+ * @version: 1.0.0 <br/>
+ */
+@SpringBootApplication
+public class MallAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
+
+}
